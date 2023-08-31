5e3627bd050f2f71ba8e7dbdc24d73929412e73c
Valentina2103 <80237175+Valentina2103@users.noreply.github.com>
2023-08-30 21:02:19 -0500

    Actualizacion No Data

--- Client/js/ListaPartidas.js
+++ Client/js/ListaPartidas.js
@@ -1,7 +1,8 @@
 let client = new Colyseus.Client('ws://game.tnb2testing.com:3000/');
 
-//Tabla en html donde se guardan las filas
+// Tabla en HTML donde se guardan las filas
 const table = document.querySelector(".tabla");
+
 const functionBusqueda = async () => {
   try {
     // Seleccionar todos los elementos con la clase "fila" y eliminarlos
@@ -15,7 +16,7 @@ const functionBusqueda = async () => {
           console.log(room.clients);
           console.log(room.maxClients);
           console.log(room.metadata);
-          //Div con clase fila donde se guardara la informacion de la partida
+          // Div con clase fila donde se guardará la información de la partida
           const tableItem = document.createElement("div");
           tableItem.classList.add("fila");
 
@@ -25,11 +26,29 @@ const functionBusqueda = async () => {
             <div class="columna">${room.metadata.ganacia}</div>
             <div class="columna"><button>Join</button></div>
           `;
-          //Div fila se guarda en la tabla
+          // Div fila se guarda en la tabla
           table.appendChild(tableItem);
         });
       } else {
         console.log("NO DATA");
+
+        // Elemento HTML para el mensaje de error
+        const errorPopup = document.createElement("div");
+        errorPopup.classList.add("popup-container");
+        errorPopup.innerHTML = `
+          <div class="popup">
+              <div class="popup-header">
+                  <span class="error-icon">!</span>
+              </div>
+              <div class="popup-content">
+                  <p>¡Ups! Algo salió mal. Por favor, intenta de nuevo más tarde.</p>
+                  <div class="btn__confirmar">
+                    <button>Confirmar</button>
+                  </div>
+              </div>
+          </div>
+        `;
+        document.body.appendChild(errorPopup);
       }
     }).catch(e => {
       console.log("JOIN ERROR", e);
@@ -41,3 +60,5 @@ const functionBusqueda = async () => {
 
 const boton_actualizar = document.getElementById('btn_actualizar');
 boton_actualizar.addEventListener('click', functionBusqueda);
+
+
