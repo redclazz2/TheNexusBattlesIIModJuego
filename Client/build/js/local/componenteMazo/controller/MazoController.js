b40e499a3e47357c9f2a4687dc2f5ce028a527af
JJ <juan.diaz.2021@upb.edu.co>
2023-09-17 01:26:19 -0500

    Añadido componente de mazo de cartas

---
+++ Client/build/js/local/componenteMazo/controller/MazoController.js
@@ -0,0 +1,11 @@
+export default class MazoController {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+        this.init = () => {
+            this.view.init();
+            this.view.cardNumber(this.model.cardCount);
+        };
+        console.log('MazoController');
+    }
+}
---
+++ Client/build/js/local/componenteMazo/model/MazoModel.js
@@ -0,0 +1,18 @@
+export default class MazoModel {
+    constructor() {
+        this.drawCard = () => {
+            if (this.mazo.length == 0)
+                return "-1";
+            else
+                return this.mazo.pop();
+        };
+        this.insertCard = (_id) => {
+            this.mazo.push(_id);
+        };
+        this.cardCount = () => {
+            return this.mazo.length;
+        };
+        console.log('MazoModel');
+        this.mazo = [];
+    }
+}
