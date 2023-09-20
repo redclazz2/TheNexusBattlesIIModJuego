9ebe80aff34f7d9e790edce56acd8797cd8b52cf
Sebastian Zarate <zaratesanmiguel2019@outlook.com>
2023-09-19 22:01:54 -0500

    Fixed an issue where the turn wouldnt continue even if out of clients

--- Server/src/rooms/RoomBattle.ts
+++ Server/src/rooms/RoomBattle.ts
@@ -48,7 +48,7 @@ export class room_battle extends Room<RoomBattleState> {
 
   handleTurnTermination = ():void =>{
     this.state.localTurnStatus++;
-      if(this.state.localTurnStatus > Number(this.state.expectedUsers)){ 
+      if(this.state.localTurnStatus == this.state.turnos.length){ 
         this.state.currentTurn++;
         this.state.localTurnStatus = 0;
       }
